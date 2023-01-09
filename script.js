d5b606330995800c3f586bc50dc4079ca82c1983
glimoire <jamiemassella@gmail.com>
2023-01-09 18:11:19 +0000

    add initial game logic: click function, only one click per cell

---
+++ script.js
@@ -0,0 +1,19 @@
+const X_CLASS = 'x'
+const O_CLASS = 'o'
+
+const cellElements = document.querySelectorAll('[data-cell]')
+let oTurn
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, {once: true})
+})
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = oTurn ? O_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
