a9be6e862892b637ea15564a1b656a617213619e
glimoire <jamiemassella@gmail.com>
2023-01-09 18:15:47 +0000

    add logic to swap turns and allow hover to take that into account

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const X_CLASS = 'x'
 const O_CLASS = 'o'
-
 const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
 let oTurn
 
 cellElements.forEach(cell => {
@@ -12,8 +12,27 @@ function handleClick(e) {
     const cell = e.target
     const currentClass = oTurn ? O_CLASS : X_CLASS
     placeMark(cell, currentClass)
+    //
+    //
+    //
+    swapTurns()
+    setBoardHoverClass()
 }
 
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    oTurn = !oTurn
+}
+
+function setBoardHoverClass () {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(O_CLASS)
+    if (circleTurn) {
+        board.classList.add(O_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
 }
