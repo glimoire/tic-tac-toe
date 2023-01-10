dce7d5ae9478013a538fa7d8feaa91444b7d3654
glimoire <jamiemassella@gmail.com>
2023-01-10 17:02:31 +0000

    add semicolons to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-const X_CLASS = 'x'
-const O_CLASS = 'o'
+const X_CLASS = 'x';
+const O_CLASS = 'o';
 const WINNING_COMBOS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -9,81 +9,81 @@ const WINNING_COMBOS = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
-const victoryMessageElement = document.getElementById('victoryMessage')
-const resetButton = document.getElementById('resetButton')
-const victoryMessageTextElement = document.querySelector('[data-victory-message-text]')
-let oTurn
+];
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const victoryMessageElement = document.getElementById('victoryMessage');
+const resetButton = document.getElementById('resetButton');
+const victoryMessageTextElement = document.querySelector('[data-victory-message-text]');
+let oTurn;
 
-startGame()
+startGame();
 
-resetButton.addEventListener('click', startGame)
+resetButton.addEventListener('click', startGame);
 
 function startGame() {
-    oTurn = false
+    oTurn = false;
     cellElements.forEach(cell => {
-        cell.classList.remove(X_CLASS)
-        cell.classList.remove(O_CLASS)
-        cell.removeEventListener('click', handleClick)
-        cell.addEventListener('click', handleClick, { once: true })
-    })
-    setBoardHoverClass()
-    victoryMessageElement.classList.remove('show')
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    victoryMessageElement.classList.remove('show');
 }
 
 function handleClick(e) {
-    const cell = e.target
-    const currentClass = oTurn ? O_CLASS : X_CLASS
-    placeMark(cell, currentClass)
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
     if (checkWin(currentClass)) {
-        endGame(false)
+        endGame(false);
     } else if (isDraw()) {
-        endGame(true)
+        endGame(true);
     } else {
-        swapTurns()
-        setBoardHoverClass()
+        swapTurns();
+        setBoardHoverClass();
     }
 }
 
 function endGame(draw) {
     if (draw) {
-victoryMessageTextElement.innerText = "It's a Draw!"
+victoryMessageTextElement.innerText = "It's a Draw!";
     } else {
-        victoryMessageTextElement.innerText = `${oTurn ? "The Noughts" : "The Crosses"} Win!`   
+        victoryMessageTextElement.innerText = `${oTurn ? "The Noughts" : "The Crosses"} Win!`;   
     }
-    victoryMessageElement.classList.add('show')
+    victoryMessageElement.classList.add('show');
 }
 
 function isDraw() {
     return [...cellElements].every(cell => {
-        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS)
-    })
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
 }
 
 function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass)
+    cell.classList.add(currentClass);
 }
 
 function swapTurns() {
-    oTurn = !oTurn
+    oTurn = !oTurn;
 }
 
 function setBoardHoverClass () {
-    board.classList.remove(X_CLASS)
-    board.classList.remove(O_CLASS)
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
     if (oTurn) {
-        board.classList.add(O_CLASS)
+        board.classList.add(O_CLASS);
     } else {
-        board.classList.add(X_CLASS)
+        board.classList.add(X_CLASS);
     }
 }
 
 function checkWin(currentClass) {
     return WINNING_COMBOS.some(combination => {
         return combination.every(index => {
-            return cellElements[index].classList.contains(currentClass)
-        })
-    })
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
 }
